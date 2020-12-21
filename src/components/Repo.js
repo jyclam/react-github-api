d0dfc73b072a3165a0962e074341b94bf98fa663
John <john.dev.work.01@gmail.com>
2020-12-20 23:38:14 -0800

    Tweak repo card styles

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { removeHyphensAndCapitalizeWithExceptions } from "../utils/index";
 
 const ArticleContainer = styled.div`
-  padding: 1rem;
+  padding: 2rem;
   margin: 1rem;
   background-color: #6aac56;
   border-radius: 0.5em;
@@ -14,18 +14,70 @@ const ArticleContainer = styled.div`
     transition-timing-function: ease-out;
     transition: 0.1s;
     transform: translateY(-2px) scale(1.05);
-    box-shadow: 0 0.5em 0.5em 0;
+    box-shadow: 0.5em 0.5em 0.5em gray;
+  }
+
+  @media screen and (max-width: 768px) {
+    padding: 1em;
+  }
+`;
+
+const StyledArticle = styled.article`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  h1 {
+    font-size: 4em;
+  }
+  h3 {
+    font-size: 2em;
+    margin-bottom: 1em;
+  }
+  p {
+    font-size: 1.5em;
+    line-height: 0.5em;
+  }
+
+  @media screen and (max-width: 768px) {
+    h1 {
+      font-size: 3em;
+    }
+
+    h3 {
+      font-size: 1.1em;
+      margin-bottom: 2em;
+    }
+    p {
+      font-size: 1em;
+    }
+  } ;
+`;
+
+const ForkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    margin-top: 0;
   }
 `;
 
 const Repo = ({ repoName, forks, language, createdAt }) => (
   <ArticleContainer>
-    <article>
-      <h3>Title: {removeHyphensAndCapitalizeWithExceptions(repoName)}</h3>
-      <p>Number of forks: {forks}</p>
-      <p>Language: {language}</p>
-      <p>Created Date: {new Date(createdAt).toLocaleDateString()}</p>
-    </article>
+    <StyledArticle>
+      <div>
+        <h3>{removeHyphensAndCapitalizeWithExceptions(repoName)}</h3>
+
+        <p>{language}</p>
+        <p>Created: {new Date(createdAt).toLocaleDateString()}</p>
+      </div>
+      <ForkContainer>
+        <h3>Forks</h3>
+        <h1>{forks}</h1>
+      </ForkContainer>
+    </StyledArticle>
   </ArticleContainer>
 );
 
